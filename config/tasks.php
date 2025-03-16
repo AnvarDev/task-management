ff86e1d15a5ae65e85b776e209fd195c472085df
AnvarDev <anwar021@yandex.com>
2025-03-16 17:49:55 +0100

    Create status & priority config

---
+++ config/tasks.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default task configs
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the task statuses and priorities.
+    |
+    */
+
+    'priority' => [
+        'Low',
+        'Medium',
+        'High',
+        'Urgent',
+    ],
+
+    'status' => [
+        'To Do',
+        'In Progress',
+        'Under Review',
+        'Completed',
+    ],
+
+];
